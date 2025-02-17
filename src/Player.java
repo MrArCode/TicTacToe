e385f316285d4f79aa71694b3e57e32f5f2f5c2b
Artur Cegiełka-Posłuszny <a.c.posluszny@gmail.com>
2025-02-17 18:34:07 +0100

    Add player class

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,45 @@
+import java.util.Scanner;
+
 public class Player {
+    private static final Scanner scanner = new Scanner(System.in);
+    private final String name;
+    private final Mark mark;
+
+    public Player(String name, Mark mark) {
+        this.name = name;
+        this.mark = mark;
+    }
+
+    public static Player createPlayer() {
+        String name = chooseName();
+        Mark mark = chooseMark();
+        return new Player(name, mark);
+    }
+
+    private static String chooseName(){
+        System.out.print("Choose a name: ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    private static Mark chooseMark() {
+        while (true) {
+            System.out.print("Choose a mark: X or O: ");
+            String mark = scanner.nextLine().trim();
+            switch (mark) {
+                case "X": return Mark.X;
+                case "O": return Mark.O;
+                default:
+                    System.out.println("Invalid mark. Please try again.");
+            }
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Mark getMark() {
+        return mark;
+    }
 }
--- src/SinglePlayer.java
+++ src/SinglePlayer.java
@@ -1,4 +1,9 @@
 public class SinglePlayer {
     private final Player player;
     private final AI ai;
+
+    public SinglePlayer(Player player, AI ai) {
+        this.player = player;
+        this.ai = ai;
+    }
 }
